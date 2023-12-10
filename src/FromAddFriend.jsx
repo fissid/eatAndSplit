6caf56da5866bc7905276a3ef60e070af1cb8572
fissid <fissid.ss@gmail.com>
2023-12-10 18:05:50 +0300

    updates

---
+++ src/FromAddFriend.jsx
@@ -0,0 +1,11 @@
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label></label>
+      <input type="text" />
+
+      <label></label>
+      <input type="text" />
+    </form>
+  );
+}
